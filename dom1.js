ce5a4d4eab99816ac66e43f9e6b1490c1022cbb2
Robyschumi <robert.andreev@yahoo.com>
2023-11-16 12:33:59 +0100

    Domasni

---
+++ dom1.js
@@ -0,0 +1,47 @@
+//prva
+// let hasCofee = true;
+// let hasTea = false;
+// let hasMilk = true;
+// console.log(hasMilk);
+// console.log(hasTea || hasMilk);
+// console.log(hasCofee && hasTea);
+// console.log(hasTea);
+// console.log(hasCofee || hasMilk) || (!hasCofee && hasMilk);
+// console.log(hasTea && hasMilk) || (hasCofee && hasTea);
+// console.log(!hasCofee && !hasTea);
+
+// vtora
+// const num1 = 10;
+// const num2 = 5;
+// const num3 = 8;
+// const num4 = 2;
+// const num5 = 12;
+// const num6 = 4;
+// const num7 = 3;
+
+// const result1 = (num1 * num5) / (num4 + num6) + (num3 - num2) / num3;
+// console.log(result1);
+// const result2 = num5 * num1 + (num7 * num3) / num2 + num6 + num4;
+// console.log(result2);
+// const result3 = num1 + num2 + num3 * num5 - num4 / (num6 + num7);
+// console.log(result3);
+// const result4 = num2 * num6 + (num3 + num1) - (num7 - num4) / num5;
+// console.log(result4);
+// const result5 = (num1 + num2 + (num5 - num4)) / num3 / (num6 + num7);
+// console.log(result5);
+
+// treta
+
+let num1 = 12;
+let num2 = 4;
+
+let sum = num1 + num2;
+let diff = num2 - num1;
+let mult = num2 * num1;
+let div = num1 / num2;
+let rem = num1 % num2;
+console.log(sum);
+console.log(diff);
+console.log(mult);
+console.log(div);
+console.log(rem);
---
+++ dom2.js
@@ -0,0 +1,53 @@
+// // prva
+// let globalNum = 10;
+
+// function checkScope() {
+//   let blockNum = 10;
+//   return blockNum;
+// }
+// console.log(globalNum === 10);
+// typeof blockNum;
+
+// //vtora
+// // a
+// let num = 10;
+
+// if (num % 3 == 0) {
+//   console.log("even number");
+// } else if (num / 2 == 5) console.log("odd number");
+
+// // b
+
+// let age = 17;
+// if (age < 18) {
+//   console.log("can't vote");
+// } else if (age >= 18) {
+//   console.log("can vote");
+// }
+
+//v
+
+// let score = 78;
+// if (score + 13 === 90) {
+//   console.log("gradeA");
+// } else if (score - 9 === 70) {
+//   console.log("gradeB");
+// } else {
+//   console.log("gradeC");
+// }
+
+//g
+
+// let dayNumber = 5;
+// switch (dayNumber) {
+//   case 2:
+//     console.log("The day is Monday");
+//     break;
+//   case 3:
+//     console.log("The day is Tuseday");
+//     break;
+//   case 4:
+//     console.log("The day is Wenesday");
+
+//     console.log(dayNumber);
+// }
